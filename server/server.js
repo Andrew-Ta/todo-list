76f1b5bb663069663814b305cc3a393632c4f543
Andrew Ta <andrew.ta@hotmail.com>
2021-11-20 15:17:01 -0700

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express()
+
+// connect database
+connectDB();
 
 // initialize middleware
 app.use(express.json({extended: false}));
