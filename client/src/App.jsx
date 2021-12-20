d0766b2ca30d81debd61d875ea3a56897f0ea076
Andrew Ta <andrew.ta@hotmail.com>
2021-12-19 17:10:36 -0700

    cleaned up code, readded 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,19 @@ import React from 'react';
 import styled from 'styled-components';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-// import HomePage from 'pages/HomePage';
 import PageNotFound from 'pages/PageNotFound';
-// import CreateTodo from 'pages/CreateTodoPage';
 import { ShowTodoList } from 'components/showTodoList';
 import { CreateTodo } from 'components/createTodo';
-
-
 
 function App() {
   return (
     <div>
       <Router>
-        {/* <Switch> */}
+        <Switch>
           <Route exact path="/" component={ShowTodoList} />          
           <Route exact path="/create-todo" component={CreateTodo} />
-          {/* <Route exact path="/"><HomePage/></Route>
-          <Route exact path="/create-todo"><CreateTodo/></Route> */}
-
-          {/* <Route path="*"><PageNotFound/></Route> */}
-        {/* </Switch> */}
+          <Route path="*"><PageNotFound/></Route>
+        </Switch>
       </Router>
     </div>
   );
